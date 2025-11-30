fbd8762e8430bd07663e97604d01cd52af64240d
Barb Miller <hello@kickstartcreatives.com>
2025-11-29 20:01:10 -0900

    Update version banner colors to navy with orange hover

--- components/UpdateNotice.tsx
+++ components/UpdateNotice.tsx
@@ -9,7 +9,7 @@ export default function UpdateNotice({ onReload }: UpdateNoticeProps) {
   return (
     <div
       onClick={onReload}
-      className="fixed bottom-5 left-1/2 -translate-x-1/2 bg-orange text-white px-6 py-4 rounded-lg shadow-lg z-[9999] cursor-pointer max-w-[90%] hover:bg-navy transition"
+      className="fixed bottom-5 left-1/2 -translate-x-1/2 bg-navy text-white px-6 py-4 rounded-lg shadow-lg z-[9999] cursor-pointer max-w-[90%] hover:bg-orange transition"
     >
       <strong>New version available!</strong> Click here to reload the page.
     </div>
